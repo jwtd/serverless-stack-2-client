283012b5a1b71300babe4e1f1589628225fde7a0
jwtd <Jordan.Duggan@Gmail.com>
2019-03-11 17:07:52 -0400

    Adding Stripe keys to config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const dev = {
+  STRIPE_KEY: "pk_test_V09IOXZiEs3xpc6GmZnSzieh",
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-2-api-dev-attachmentsbucket-l3i9krhw7job"
@@ -16,6 +17,7 @@ const dev = {
 };
 
 const prod = {
+  STRIPE_KEY: "pk_test_V09IOXZiEs3xpc6GmZnSzieh",
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-2-api-prod-attachmentsbucket-1f8lgfqzzwnwn"
